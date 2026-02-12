983416ca37128d4db8a877ae73e7091b83537f6a
OwennXzz <w.inaksorn@gmail.com>
2026-02-12 23:40:22 +0700

    merge Authentication-Feature and Notification-Feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:score_management/Authentication/loginPage.dart';
-import 'package:score_management/Notification/notificationPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Notificationpage(),
+      home: LoginPage(),
     );
   }
 }
